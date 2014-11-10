a23ca8e84d3eb78a57d37f9e189c11e5af6fd5d3
wazyu <g1344630@cc.kyoto-su.ac.jp>
2014-11-10 19:45:11 +0900

    change

--- HelloJava.java
+++ HelloJava.java
@@ -1,12 +1,6 @@
 public class HelloJava{
     public static void main(String[] args){
-	HelloJavaObject app1=new HelloJavaObject();
-	HelloJavaObject app2=new HelloJavaObject();
-	int num=Integer.parseInt(args[0]);
-	for(int i=1;i<=num;i++){
-	    app1.sayHello();
-	    app2.sayHello();
-	}
+	System.out.println("Hello Java!");
     }
 }
 
